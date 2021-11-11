98ebe5552abdb76417a57e6adc5b70c2a87b8e57
pspapleng <62070039@kmitl.ac.th>
2021-11-11 23:16:56 +0700

    New : Connecting to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MembersModule } from './members/members.module';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [MembersModule],
+  imports: [
+   TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
