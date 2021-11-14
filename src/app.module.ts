4238487c53cdec21b3fabfe01a50d1c1c1a8a5e2
pspapleng <62070039@kmitl.ac.th>
2021-11-15 01:10:20 +0700

    New : create members module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configService } from './config/config.service';
 import { ItemModule } from './item/item.module';
+import { MembersModule } from './members/members.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig()), ItemModule
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()), ItemModule, MembersModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -29,6 +29,10 @@ class ConfigService {
     return mode != 'DEV';
   }
 
+  public getSecret() {
+    return 'MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgH+9kFPdeZ7lKHaLP4+DEIYNw7b2E4o2b3gyIxjlGsjvWiymQ3PkOZZhENr6MtUkNph9OyVWnmA1wsYkzUXxCZsvPFgD5DLtJ0X';
+  }
+
   public getTypeOrmConfig(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
