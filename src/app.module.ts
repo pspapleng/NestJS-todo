ceca6d4231fb48cc1e2647370f1138235cf11b31
pspapleng <62070039@kmitl.ac.th>
2021-11-16 00:27:17 +0700

    Chore : clear item module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configService } from './config/config.service';
-import { ItemModule } from './item/item.module';
 import { MembersModule } from './members/members.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig()), ItemModule, MembersModule
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()), MembersModule
   ],
   controllers: [AppController],
   providers: [AppService],
