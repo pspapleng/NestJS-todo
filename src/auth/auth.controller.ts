90f6c12621837efa16eba87e254d8f9cd6276979
pspapleng <62070039@kmitl.ac.th>
2021-11-17 01:00:45 +0700

    Update : login with JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,12 @@ export class AuthController {
     @UseGuards(LocalAuthGuard)
     @Post('login')
     async login(@Request() req) {
+        return this.authService.login(req.user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
         return req.user;
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,14 @@ import { configService } from '../config/config.service';
 import { AES, enc } from 'crypto-js';
 import { MembersDTO } from '../members/members.dto';
 import { MembersService } from '../members/members.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private membersService: MembersService) { }
+    constructor(
+        private membersService: MembersService,
+        private jwtService: JwtService
+    ) { }
 
     async validateUser(username: string, password: string): Promise<any> {
         const member = await this.membersService.getByUsername(username)
@@ -14,5 +18,12 @@ export class AuthService {
             return MembersDTO.fromEntity(member);
         }
         return null;
+    }
+
+    async login(member: any) {
+        const payload = { username: member.username, sub: member.userId };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
     }
 }
